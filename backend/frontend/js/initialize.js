92fd9559c1c229dd5c3fd06aaafe49da4768b840
selrach577 <144854315+selrahc577@users.noreply.github.com>
2023-10-08 01:26:52 -0500

    Trying initialization using separate functions

--- backend/frontend/js/initialize.js
+++ backend/frontend/js/initialize.js
@@ -1,13 +1,13 @@
 import { Map, Room, Hallway } from './gamemap.mjs';
 
-export function initializeGame()
+export function initializeGame(app)
 {
-	createMap();
+	createMap(app);
 }
 
-function createMap() {
+function createMap(app) {
 	// for i in i
-	createRoom('room1');
+	//createRoom('room1');
 	// add room to 
 	// create 9 squares
 	// each square is centered on a multiple of 1/3 of the length and width
@@ -15,6 +15,81 @@ function createMap() {
 	// create passageways
 	//const map = new Map(rooms, hallways);
 	//return map;
+// screen width
+	const Sw = app.renderer.view.width;
+	// screen height
+	const Sh = app.renderer.view.height;
+	// map buffer
+	const Mb = 25;
+	// map width
+	const Mw = Sw - 2 * Mb;
+	// map height
+	const Mh = Sh - 2 * Mb;
+	// room edge
+	const Re = Mh/5;
+	// hallway ( long side )
+	const Hl = Re;
+	// hallway ( short side )
+	const Hs = Re/3;
+
+	const room_coordinates = [
+		[ Mb, Mb ],
+		[ Mb, Mb + Re + Hl ],
+		[ Mb, Mb + 2 * ( Re + Hl ) ],
+		[ Mb + Re + Hl, Mb ],
+		[ Mb + Re + Hl, Mb + Re + Hl ],
+		[ Mb + Re + Hl, Mb + 2 * ( Re + Hl ) ],
+		[ Mb + 2 * ( Re + Hl ), Mb ],
+		[ Mb + 2 * ( Re + Hl ), Mb + Re + Hl ],
+		[ Mb + 2 * ( Re + Hl ), Mb + 2 * ( Re + Hl ) ]
+	];
+
+	const hallway_across_coordinates = [
+		[ Mb + Re, Mb + Re/3 ],
+		[ Mb + Re, Mb + Re/3 + Hl + Re ],
+		[ Mb + Re, Mb + Re/3 + 2 * ( Hl + Re ) ],
+		[ Mb + Re + Hl + Re, Mb + Re/3 ],
+		[ Mb + Re + Hl + Re, Mb + Re/3 + Hl + Re ],
+		[ Mb + Re + Hl + Re, Mb + Re/3 + 2 * ( Hl + Re ) ],
+	];
+
+	const hallway_down_coordinates = [
+		[ Mb + Re/3, Mb + Re ],
+		[ Mb + Re/3, Mb + Hl + ( 2 * Re ) ],
+		[ Mb + Re/3 + Hl + Re, Mb + Re ],
+		[ Mb + Re/3 + Hl + Re, Mb + Re + Hl + Re ],
+		[ Mb + Re/3 + 2 * ( Hl + Re ), Mb + Re ],
+		[ Mb + Re/3 + 2 * ( Hl + Re ), Mb + Re + Hl + Re ]
+	];
+
+	let room_rectangles = [];
+	room_coordinates.forEach((room) => {
+		const rectangle = new PIXI.Graphics();
+		rectangle.beginFill(0x66CCFF);
+		rectangle.drawRect(room[0],room[1],Re,Re);
+		rectangle.endFill();
+		app.stage.addChild(rectangle);
+		room_rectangles.push(rectangle);
+	});
+
+	let hallway_rectangles = [];
+	hallway_across_coordinates.forEach((hallway) => {
+		const rectangle = new PIXI.Graphics();
+		rectangle.beginFill(0x66CCFF);
+		rectangle.drawRect(hallway[0],hallway[1],Hl,Hs);
+		rectangle.endFill();
+		app.stage.addChild(rectangle);
+		hallway_rectangles.push(rectangle);
+	});
+
+	hallway_down_coordinates.forEach((hallway) => {
+		const rectangle = new PIXI.Graphics();
+		rectangle.beginFill(0x66CCFF);
+		rectangle.drawRect(hallway[0],hallway[1],Hs,Hl);
+		rectangle.endFill();
+		app.stage.addChild(rectangle);
+		hallway_rectangles.push(rectangle);
+	});
 }
 
 function createRoom(name) 
