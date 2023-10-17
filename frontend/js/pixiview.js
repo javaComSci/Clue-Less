794d028f55afbc98bfcc96b9fa39984217b14fc4
selrach577 <144854315+selrahc577@users.noreply.github.com>
2023-10-17 06:56:12 -0500

    Frontend: Displays Rooms and Hallways

--- frontend/js/pixiview.js
+++ frontend/js/pixiview.js
@@ -9,6 +9,20 @@ export class PixiMap extends GameMap
 		super.createMap();
 	}
 
+	// TODO: Create a parent container that holds the rooms on the pixiMap
+	/*
+	displayRooms() {
+		const rooms = new PIXI.Graphics();
+		for(var room in this.rooms) {
+			const rectangle = new PIXI.Rectangle(this.rooms[room].x,this.rooms[room].y,this.rooms[room].length,this.rooms[room].width);
+			rooms.beginFill(0x66CCFF);
+			// https://pixijs.download/release/docs/PIXI.Graphics.html#drawShape
+			rooms.drawShape(rectangle);
+			rooms.endFill();
+			this.app.stage.addChild(rectangle);
+		}
+	}
+	*/
 	displayRooms() {
 		for(var room in this.rooms) {
 			const rectangle = new PIXI.Graphics();
@@ -16,9 +30,14 @@ export class PixiMap extends GameMap
 			rectangle.drawRect(this.rooms[room].x,this.rooms[room].y,this.rooms[room].length,this.rooms[room].width);
 			rectangle.endFill();
 			this.app.stage.addChild(rectangle);
-			//this.room_rectangles.push(rectangle);
+			this.rooms[room].element = rectangle;
+			const roomName = new PIXI.Text(room, { fontSize: 24, fill: 0xFBF8FB });
+			roomName.x = this.rooms[room].x;
+			roomName.y = this.rooms[room].y;
+			rectangle.addChild(roomName);
 		}
 	}
+	// TODO: Create a parent container that holds the hallways on the pixiMap
 	displayHallways() {
 		for(var hallway in this.hallways) {
 			const rectangle = new PIXI.Graphics();
@@ -26,7 +45,9 @@ export class PixiMap extends GameMap
 			rectangle.drawRect(this.hallways[hallway].x,this.hallways[hallway].y,this.hallways[hallway].length,this.hallways[hallway].width);
 			rectangle.endFill();
 			this.app.stage.addChild(rectangle);
-			//this.hallway_rectangles.push(rectangle);
+			this.hallways[hallway].element = rectangle;
 		}
+	}
+	displayPlayers() {
 	}
 }
